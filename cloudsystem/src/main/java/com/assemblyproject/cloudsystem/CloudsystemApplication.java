f0aa4ec8da28406984e8c2286ccfce1c9b92cff3
evgueny_nenane <evgueninenane73@gmail.com>
2026-09-07 00:57:35 +0200

    feat: starting Spring Boot

---
+++ cloudsystem/src/main/java/com/assemblyproject/cloudsystem/CloudsystemApplication.java
@@ -0,0 +1,13 @@
+package com.assemblyproject.cloudsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudsystemApplication.class, args);
+	}
+
+}
