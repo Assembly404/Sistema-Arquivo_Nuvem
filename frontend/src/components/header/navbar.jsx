f3e8eeec897227142c2f0edd14a8b1fdd5840d78
Malik-Mangue <mlktecno@gmail.com>
2026-09-22 18:34:36 +0200

    Criando estrutura de NavBar

---
+++ frontend/src/components/header/navbar.jsx
@@ -0,0 +1,114 @@
+import "./navbar.css"
+import { Link } from "react-router"
+
+export default function NavBar(){
+    return(
+        <><header>
+            
+                <nav>
+                    <div>
+                        {/* Logo do Sistema */}
+                    </div>
+                    <div>
+                        {/* Os itens da landing page */}
+                    </div><li>
+                                {/* Logo do Sistema */}
+                            </li>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li></li>
+                            <li>
+                                <Link to={'/login'}>
+                                    Sign in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/register'}>
+                                    Sign up
+                                </Link>
+                            </li>   <li>
+                             <li>
+                             <li>
+                             <li>
+                             <li>
+                                                                                                                                                                                                                                                                {/* Logo do Sistema */}
+                            </li>
+                            <li>Home</li>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                            <li>About Us</li>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                            <li></li>
+                            <li>
+                                <Link to={'/login'}>
+                                    Sign in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/register'}>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                                    Sign up
+                                </Link>
+                            </li>      {/* Logo do Sistema */}
+                            </li>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li></li>
+                            <li>
+                                <Link to={'/login'}>
+                                    Sign in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/register'}>
+                                    Sign up
+                                </Link>
+                            </li>      {/* Logo do Sistema */}
+                            </li>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li></li>
+                            <li>
+                                <Link to={'/login'}>
+                                    Sign in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/register'}>
+                                    Sign up
+                                </Link>
+                            </li>      {/* Logo do Sistema */}
+                            </li>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li></li>
+                            <li>
+                                <Link to={'/login'}>
+                                    Sign in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/register'}>
+                                    Sign up
+                                </Link>
+                            </li>      {/* Logo do Sistema */}
+                            </li>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li></li>
+                            <li>
+                                <Link to={'/login'}>
+                                    Sign in
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/register'}>
+                                    Sign up
+                                </Link>
+                            </li>   
+                    <div>
+                        {/* Os botoes de Sign in e Sign upv */}
+                    </div>
+                         
+                </nav>
+        </header>
+        </>
+    )
+
+}
